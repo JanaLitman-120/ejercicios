1c904de29061c8d62801162182bb347dfaf76140
“JanaLitman-120” <janilit26@gmail.com>
2023-04-09 02:07:28 -0300

    El primer commit

---
+++ Pruebacarpetaprog.java
@@ -0,0 +1,16 @@
+/*
+ probando
+ */
+package pruebacarpetaprog;
+
+/**
+ *
+ * @author User
+ */
+public class Pruebacarpetaprog {
+
+    public static void main(String[] args) {
+        System.out.println("Hola espero que funcione");
+    }
+    
+}
